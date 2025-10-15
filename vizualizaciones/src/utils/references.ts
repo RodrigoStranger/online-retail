926081ad75ead0cfe0c30927c3d61a7fb8111227
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-10-15 14:25:36 -0500

    update

--- vizualizaciones/src/utils/references.ts
+++
@@ -1,9 +0,0 @@
-interface referencesType {
-  github: string;
-  ulasalle: string;
-}
-
-export const references: referencesType = {
-  github: "https://github.com/RodrigoStranger",
-  ulasalle: "https://www.ulasalle.edu.pe/"
-};
